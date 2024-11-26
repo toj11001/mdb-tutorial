2e6d5d5d4ab5353f47cd908e4486e998f6a7dc2a
Tobias <tobias.joschko@gmx.de>
2024-11-26 16:15:46 +0100

    add copyright

--- k8s/cli/cmd/root.go
+++ k8s/cli/cmd/root.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 Tobias Joschko <tobias.joschko@mongodb.com>
 */
 package cmd
 
@@ -9,8 +8,6 @@ import (
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
--- k8s/cli/main.go
+++ k8s/cli/main.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 Tobias Joschko <tobias.joschko@mongodb.com>
 */
 package main
 
